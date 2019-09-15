73ef0265e5ca9bad86cce926af460183d1cb9f48
Manish <Manish@LAPTOP-ND6Q0V7S>
2019-09-15 15:13:34 +0530

    firstcommit

---
+++ launchchrome/src/main/java/com/test/launchchrome/App.java
@@ -0,0 +1,13 @@
+package com.test.launchchrome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ launchchrome/src/test/java/com/test/launchchrome/AppTest.java
@@ -0,0 +1,20 @@
+package com.test.launchchrome;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest{
+	
+	@Test
+	public void login() throws InterruptedException {
+	System.out.println("Login into");
+	String baseurl = "https://www.facebook.com/";
+	WebDriver driver;
+	System.setProperty("webdriver.chrome.driver", "H:/Selenium/Downloads/chromedriver_win32/chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get(baseurl);
+	Thread.sleep(1000);
+	driver.quit();
+	
+}
+}
